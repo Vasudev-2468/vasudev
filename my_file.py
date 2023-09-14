7e87373e9df8cff5b3b0941736769ef85328365f
Vasudev-2468 <svasudevanidvrs@gmail.com>
2023-09-14 19:35:49 +0530

    drink water

--- my_file.py
+++ my_file.py
@@ -2,4 +2,7 @@ input = input("Is Vasu had the food ?")
 if input == 'yes':
     print('It good')
 else:
-    print('Ask him to eat')
+    input = input('If he needs water ?')
+    if input == 'yes':
+        print('Ask him to drink water')
+    
