d6fba0a4a21a583f8af7f542ecb0a129fece2bc9
Vasudev-2468 <svasudevanidvrs@gmail.com>
2023-09-14 19:41:46 +0530

    dosa and juice

--- my_file.py
+++ my_file.py
@@ -1,5 +1,8 @@
 input = input("Is Vasu had the food ?")
 if input == 'yes':
     print('It good')
+    print('He is good to eat dosa')
+elif input == 'no':
+    print('Ask him to drink juice')
 else:
     print('Ask him to eat')
